de16801e1b0969a132f31ce93aebc448d1044470
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-01 10:14:14 +0000

    showリンク作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
+  
   end
 
   def index
@@ -15,7 +16,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(id: params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
