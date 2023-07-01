82f83b538a2bec981e0afbaca74899bfea44e4b7
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-01 04:57:32 +0000

    [start]3-15

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+  def  new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(id: params[:id])
+  end
+
+  def edit
+    @book = Book.all
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
