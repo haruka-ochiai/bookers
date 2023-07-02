d8ac69d1dfce2abfa85fccd402c404954a2a3fd4
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-02 01:05:26 +0000

    バリデーション機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,13 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -20,7 +23,19 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.all
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
