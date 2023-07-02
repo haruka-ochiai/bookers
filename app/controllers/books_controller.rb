e88df522bd7f988a9a3122ff8d0aa49933f3d15c
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-02 06:14:09 +0000

    リンクOK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -16,6 +17,9 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    
+    
+    
   end
 
   def show
@@ -29,12 +33,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   
